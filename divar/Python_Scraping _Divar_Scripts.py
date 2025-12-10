eeb703a3a5b5ac728ff57670713d28f46c61aa52
shahramsamar <shahramsamar2010@gmail.com>
2025-12-10 23:39:06 +0330

    Update Python_Scraping _Divar_Scripts.py

--- divar/Python_Scraping _Divar_Scripts.py
+++ divar/Python_Scraping _Divar_Scripts.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 req = requests.get("https://divar.ir/s/shiraz/motorcycles")
 soup = BeautifulSoup(req.text, "html.parser")
 variable = soup.find_all(class_="kt-post-card__body")
+
 for i in variable:
     if "توافق" in i.text:
         print(i.tex)
